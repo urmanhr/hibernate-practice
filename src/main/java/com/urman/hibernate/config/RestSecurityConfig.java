bc99f04fa17c2cdf226ff7e00f79abfedfd7469d
Urman Ratneshwar <urmanhr@gmail.com>
2022-07-20 21:16:25 +0530

    adding basic auth for jersey

--- src/main/java/com/urman/hibernate/config/RestSecurityConfig.java
+++ src/main/java/com/urman/hibernate/config/RestSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.urman.hibernate.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,6 +10,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable();
+	  http
+      .csrf().disable()
+      .authorizeRequests().anyRequest().authenticated()
+      .and()
+      .httpBasic();
+  }
+  
+  @Autowired(required = false)
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+      auth
+          .inMemoryAuthentication()
+          .withUser("user").password("password").roles("USER").and()
+          .withUser("admin").password("password").roles("USER", "ADMIN");
   }
 }
